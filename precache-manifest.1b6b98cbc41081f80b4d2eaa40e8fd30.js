cd188889421539e4ad6eb5ed0f135f9660faf0fe
Diaz J. Mauricio <ryuma.diaz@gmail.com>
2019-07-26 22:35:07 -0300

    Updates

---
+++ precache-manifest.1b6b98cbc41081f80b4d2eaa40e8fd30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a649ca4d1fdb85b7b8559f3606c4ff6b",
+    "url": "/LoginTextSize/index.html"
+  },
+  {
+    "revision": "0fa71fde70ffee25faf7",
+    "url": "/LoginTextSize/static/css/main.267f05fd.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/LoginTextSize/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "0fa71fde70ffee25faf7",
+    "url": "/LoginTextSize/static/js/main.df00236f.chunk.js"
+  },
+  {
+    "revision": "53d5d56c564c31d66fae",
+    "url": "/LoginTextSize/static/js/runtime~main.706bae48.js"
+  }
+]);
